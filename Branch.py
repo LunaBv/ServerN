6bf6ae73a39f22cd1521de99cebbaad6d6465807
LunaBv <169060333+LunaBv@users.noreply.github.com>
2024-07-17 06:19:49 +0000

    Agregar todos los archivos nuevos o modificados

--- Branch.py
+++ Branch.py
@@ -1,11 +1,16 @@
 import subprocess
+import os
+from colorama import Fore, Style, init
+
+# Inicializar colorama
+init(autoreset=True)
 
 # Función para ejecutar comandos sin mostrar salida
 def run_command(command):
-    subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.run(command, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 # Solicitar el nombre del branch al usuario
-new_branch_name = input("Ingresa el nombre del branch: ")
+new_branch_name = input(Fore.CYAN + "Ingresa el nombre del branch: " + Style.RESET_ALL)
 commit_message = "Agregar todos los archivos nuevos o modificados"
 
 # Obtener información del repositorio
@@ -29,5 +34,5 @@ run_command(["git", "push", "origin", new_branch_name])
 
 # Imprimir el nombre del branch y el enlace
 branch_link = f"https://github.com/{username}/{repo_name}/tree/{new_branch_name}"
-print(f"Branch creado: {new_branch_name}")
-print(f"Enlace al branch: {branch_link}")
+print(Fore.GREEN + f"\nBranch creado: {new_branch_name}")
+print(Fore.GREEN + f"Enlace al branch: {branch_link}" + Style.RESET_ALL)
